0d4b68f843e3fecbf3d4e78c661a3b715b537b96
Carlotta <c.fabian@turbit.de>
2023-12-03 09:20:15 +0100

    template

---
+++ template.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input= include_str!("./input.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    let output = input.lines().map(process_line).sum::<u32>();
+    output
+}
+
+fn process_line(&str) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let result = part1("");
+        assert_eq!(result, "".to_string());
+    }
+}
